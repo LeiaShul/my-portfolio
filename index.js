b58853427ed318118f6a2d4e33af30bfd02570c1
LeiaShul <lesia.shulezhko24@gmail.com>
2022-12-16 11:10:30 +0200

    added text for react weather app

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let weatherImg = document.querySelector("#weather-img");
 let weatherReactLink = document.querySelector("#weather-react-link");
-let weatherLink = document.querySelector("#weather-link")
+let weatherLink = document.querySelector("#weather-link");
+let weatherText = document.querySelector("#weather-text");
 
 function WeatherReactLink(event) {
     event.preventDefault();
     weatherImg.innerHTML = `<img src="images/weather-react.png" alt="Weather project preview" class="img-fluid rounded" />`
+    weatherText.innerHTML = "A fully-functioning weather app built using HTML, CSS, advanced JavaScript, React, Bootstrap, API's and real-life development workflow. It's open-sourced and hosted on Netlify."
 }
 
 function WeatherLink(event) {
     event.preventDefault();
     weatherImg.innerHTML = `<img src="images/weather.png" alt="Weather project preview" class="img-fluid rounded" />`
+    weatherText.innerHTML = "A fully-functioning weather app built using HTML, CSS, advanced JavaScript, Bootstrap, API's and real-life development workflow. It's open-sourced and hosted on Netlify."
 
 }
 
